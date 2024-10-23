792c0a3c1c8c99ee7c82d4c37fa3a001c5f7868a
Karim <gtder41@gmail.com>
2024-10-23 19:55:33 +0500

    add files

---
+++ CreateX_proj/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+
+import './App.css'
+import Info from './components/Info/Info'
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Info/>
+    </>
+  )
+}
+
+export default App
---
+++ CreateX_proj/src/components/Info/Info.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Info.css'
+export default function Info() {
+  return (
+	<div className='info_block'>
+		<h3>
+		Createx SEO Agency is a full-service digital marketing agency. We help businesses make more money through a wealth of performance data and market research. We create science-based SEO strategies to empower our clients.
+		</h3>
+		<img src='./public/img/awards.png'/>
+
+		<img className='img_clients' src='./public/img/Clients.png'/>
+	</div>
+  )
+}
